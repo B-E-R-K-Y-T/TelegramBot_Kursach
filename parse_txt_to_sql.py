ad957dcba523439765df466bda6f5ef9d11bcf9b
Molodoy <alexchase666@gmail.com>
2021-11-10 12:53:29 +0300

    11

--- parse_txt_to_sql.py
+++ parse_txt_to_sql.py
@@ -1,3 +1,6 @@
 import parse_files
 
-
+parse_files.replace_file('data/dialogs_vk/dialog2.txt', 'Berkyt Berk [b_e_r_k_y_t]', '')
+parse_files.replace_file('data/dialogs_vk/dialog2.txt', 'Аполлинария Хорош [casper_ff]', '')
+parse_files.del_space('data/dialogs_vk/dialog2.txt')
+parse_files.split_file('data/dialogs_vk/dialog2.txt')
