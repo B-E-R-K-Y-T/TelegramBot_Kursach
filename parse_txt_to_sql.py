ec5fd77e69f4f71a7ce0351f1c89d0ae2a4a8e0b
B-E-R-K-Y-T <65071228+B-E-R-K-Y-T@users.noreply.github.com>
2021-11-10 13:44:55 +0300

    Update file: ([parse_txt_to_sql.py])

--- parse_txt_to_sql.py
+++ parse_txt_to_sql.py
@@ -1,11 +1,11 @@
 import parse_files
 
+# Список строк, кторые надо заменить
+list_str_to_replace = ['Berkyt Berk [b_e_r_k_y_t]', 'Аполлинария Хорош [casper_ff]']
 
-L = ['Berkyt Berk [b_e_r_k_y_t]', 'Аполлинария Хорош [casper_ff]']
-# L1 = ['Проверка']
-# s = 'Проверка, является ли replace_from списком строк или строкой'
-# print(s.replace(L1[0], ''))
-parse_files.replace_file('data/dialogs_vk/dialog2.txt', L, '', True)
-# parse_files.replace_file('data/dialogs_vk/dialog2.txt', 'Berkyt Berk [b_e_r_k_y_t]', '')
-# parse_files.del_space('data/dialogs_vk/dialog2.txt')
-# parse_files.split_file('data/dialogs_vk/dialog2.txt')
+# Заменяет строки
+parse_files.replace_file('data/dialogs_vk/dialog2.txt', list_str_to_replace, '', True)
+# Удаляет пробелы
+parse_files.del_space('data/dialogs_vk/dialog2.txt')
+# Разделяет файлы на ответы и вопросы
+parse_files.split_file('data/dialogs_vk/dialog2.txt')
